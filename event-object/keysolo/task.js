f0c7d9301501d1ae1b9e769f41a0376e63ed6b26
Nadezhda Zizevskaya <7_nadezhda@mail.ru>
2021-05-01 20:22:18 +0400

    first commit keysolo

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -1,90 +1,94 @@
 class Game {
-  constructor(container) {
-    this.container = container;
-    this.wordElement = container.querySelector('.word');
-    this.winsElement = container.querySelector('.status__wins');
-    this.lossElement = container.querySelector('.status__loss');
+   constructor(container) {
+      this.container = container;
+      this.wordElement = container.querySelector('.word');
+      this.winsElement = container.querySelector('.status__wins');
+      this.lossElement = container.querySelector('.status__loss');
 
-    this.reset();
-
-    this.registerEvents();
-  }
-
-  reset() {
-    this.setNewWord();
-    this.winsElement.textContent = 0;
-    this.lossElement.textContent = 0;
-  }
-
-  registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
-  }
-
-  success() {
-    this.currentSymbol.classList.add('symbol_correct');
-    this.currentSymbol = this.currentSymbol.nextElementSibling;
-    if (this.currentSymbol !== null) {
-      return;
-    }
-
-    if (++this.winsElement.textContent === 10) {
-      alert('Победа!');
       this.reset();
-    }
-    this.setNewWord();
-  }
 
-  fail() {
-    if (++this.lossElement.textContent === 5) {
-      alert('Вы проиграли!');
-      this.reset();
-    }
-    this.setNewWord();
-  }
+      this.registerEvents();
+   }
 
-  setNewWord() {
-    const word = this.getWord();
+   reset() {
+      this.setNewWord();
+      this.winsElement.textContent = 0;
+      this.lossElement.textContent = 0;
+   }
 
-    this.renderWord(word);
-  }
+   registerEvents() {
+      /*
+        TODO:
+        Написать обработчик события, который откликается
+        на каждый введённый символ.
+        В случае правильного ввода слова вызываем this.success()
+        При неправильном вводе символа - this.fail();
+       */
+      document.addEventListener('keydown', (event) => {
+         this.currentSymbol.textContent.toLowerCase() == event.key.toLowerCase() ?
+            this.success() :
+            this.fail();
+      });
+   }
 
-  getWord() {
-    const words = [
-        'bob',
-        'awesome',
-        'netology',
-        'hello',
-        'kitty',
-        'rock',
-        'youtube',
-        'popcorn',
-        'cinema',
-        'love',
-        'javascript'
-      ],
-      index = Math.floor(Math.random() * words.length);
+   success() {
+      this.currentSymbol.classList.add('symbol_correct');
+      this.currentSymbol = this.currentSymbol.nextElementSibling;
+      if (this.currentSymbol !== null) {
+         return;
+      }
 
-    return words[index];
-  }
+      if (++this.winsElement.textContent === 10) {
+         alert('Победа!');
+         this.reset();
+      }
+      this.setNewWord();
+   }
 
-  renderWord(word) {
-    const html = [...word]
-      .map(
-        (s, i) =>
-          `<span class="symbol ${i === 0 ? 'symbol_current': ''}">${s}</span>`
-      )
-      .join('');
-    this.wordElement.innerHTML = html;
+   fail() {
+      if (++this.lossElement.textContent === 5) {
+         alert('Вы проиграли!');
+         this.reset();
+      }
+      this.setNewWord();
+   }
 
-    this.currentSymbol = this.wordElement.querySelector('.symbol_current');
-  }
+   setNewWord() {
+      const word = this.getWord();
+
+      this.renderWord(word);
+   }
+
+   getWord() {
+      const words = [
+            'bob',
+            'awesome',
+            'netology',
+            'hello',
+            'kitty',
+            'rock',
+            'youtube',
+            'popcorn',
+            'cinema',
+            'love',
+            'javascript'
+         ],
+         index = Math.floor(Math.random() * words.length);
+
+      return words[index];
+   }
+
+   renderWord(word) {
+      const html = [...word]
+         .map(
+            (s, i) =>
+            `<span class="symbol ${i === 0 ? 'symbol_current': ''}">${s}</span>`
+         )
+         .join('');
+      this.wordElement.innerHTML = html;
+
+      this.currentSymbol = this.wordElement.querySelector('.symbol_current');
+   }
 }
 
 new Game(document.getElementById('game'))
-
